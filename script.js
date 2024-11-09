73f7e9c728e243a68742cba4d9eea4a57214dc35
Ezuri6 <carlotacharlot@hotmail.com>
2024-11-09 12:44:11 +0000

    moved css and js to their files

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert("Hi!");
+let name = prompt("What is your name?");
+      name = name.trim();
+      if (name.length > 0) {
+        alert(`Welcome to this adventure, ${name}! 🐢`);
+      } else {
+        alert("Too bad, we cannot proceed without your name!🦀");
+      }
